68e5f46118c37d97efc551239e8bb0c0f6a25bdd
shaunhailey <ediblelysol@gmail.com>
2017-08-29 18:07:39 -0400

    added some to data component

--- src/components/Burgers.js
+++ src/components/Burgers.js
@@ -1,2 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const Burgers = () => {
+  return (
+    <div className="burgers">
+      <h2>Enjoy the Worste Burgers!</h2>
+    </div>
+  )
+}
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,2 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const burgerItems = [
+  {
+    Basic_burger: './Images/bad_burger.jpg'
+  },
+  {
+    Hippo_burger: './Images/hippo_burger.jpg'
+  },
+  {
+    Worste_burger: './Images/the-worst-burger-ever.jpg'
+  }
+]
+
+const cameraItems = [
+  {
+    Big_camera: './Images/bad_camera.jpeg'
+  },
+  {
+    Costume_camera: './Images/camera-costume-1.jpg'
+  },
+  {
+    Tat_camera: './Images/embedded_camera_arm_tattoo.jpg'
+  }
+]
+
+const insuranceItems = [
+  {
+    Bad_insurance: './Images/bad_insurance_guy.png'
+  },
+  {
+    Kaufmann_insurance: './Images/employee_kauffman.jpg'
+  },
+  {
+    Skeevy_insurance: './Images/poor_insurance_guy.jpeg'
+  }
+]
+
+const VHSItems = [
+  {
+    Birdemic_movie: './Images/worst_movie_birdemic.jpg'
+  },
+  {
+    Ice_movie: './Images/worst_movie_ice.jpg'
+  },
+  {
+    Vape_movie: './Images/worst_movie_vapes.jpg'
+  }
+]
+
+const staff = [(Eyebrows_staff: './Images/worst_employee_ever.jpg'), (Burger_staff: './Images/worst_employee.jpg')]
